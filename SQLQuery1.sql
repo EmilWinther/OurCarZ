340fa064d1e638bfea2c0bef7a0ef4fb0f390fb0
MadsBruhn <mads80i4@edu.easj.dk>
2021-04-29 10:59:30 +0200

    Rating

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿SELECT SUM(rating) As RatingSum, COUNT(*) As RatingCount FROM Rating 
+WHERE UserID = 6
+
+SELECT COUNT(*) As RatingCount FROM Rating
+WHERE RouteID = 2 AND UserID = 6
+
+INSERT INTO Rating (rating, UserID, RouteID) 
+VALUES ( 3, 7, 2);
+
+SELECT COUNT(*) As RatingCount FROM Rating
+WHERE RouteID = 2 AND UserID = 7
