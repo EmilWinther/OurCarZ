7a588d3df244aa8254ea565de5d99c5b0ff14e39
ThomasSchou <thom63t4@edu.easj.dk>
2021-04-29 11:17:00 +0200

    Added Javascript Route compatability

--- OurCarZ/Pages/Route.cshtml.cs
+++ OurCarZ/Pages/Route.cshtml.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OurCarZ.Model;
 
 namespace OurCarZ.Pages
 {
     public class RouteModel : PageModel
     {
-        public string StartPoint = "Ahornlunden 5, DK";
-        public string FinishPoint = "Maglegårdsvej 2, Roskilde, DK";
 
-        public void OnGet()
+        [BindProperty]
+        public Route route{ get; set; }
+        [BindProperty]
+        public string StartPoint { get; set; }
+        [BindProperty]
+        public string FinishPoint { get; set; }
+
+        public void OnGet(int id)
         {
+            //Ret nedenstående til den rigtige kode
+            //route = RouteService.GetOneRoute(id);
+            //route.StartPoint = StartPoint;
+            //route.FinishPoint = FinishPoint;
 
         }
+
+
     }
 }
