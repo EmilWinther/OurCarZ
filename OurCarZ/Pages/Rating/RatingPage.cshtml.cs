f79c77a26f0e85ab0561acab292b379c1c57482f
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-05-04 13:09:52 +0200

    Validation check (Kinda?) for RatingPage, and a temporary link to the page on Index.

--- OurCarZ/Pages/Rating/RatingPage.cshtml.cs
+++ OurCarZ/Pages/Rating/RatingPage.cshtml.cs
@@ -26,20 +26,33 @@ namespace OurCarZ.Pages.Rating
         }
         public IActionResult OnPost() 
         {
-            //Check if the specified Composite Key already exists in the Database and update it
-            if (DB.RatingDatabases.Find(UserToBeRated, UserRating) != null)
+            //Making sure nothing is zero, because it will break the program if true. Luckily, the integers will also return 0
+            //if an illegal character is input on the page (such as letters)
+            if (UserToBeRated != 0 && UserRating != 0 && Rating != 0)
             {
-                var Rater = DB.RatingDatabases.FirstOrDefault(b => b.UserRatedId == UserToBeRated && b.UserRatingId == UserRating);
-                Rater.Rating = Rating;
-                DB.RatingDatabases.Update(Rater);
+                //Check if the specified Composite Key already exists in the Database and update it
+                if (DB.RatingDatabases.Find(UserToBeRated, UserRating) != null)
+                {
+                    var Rater = DB.RatingDatabases.FirstOrDefault(b => b.UserRatedId == UserToBeRated && b.UserRatingId == UserRating);
+                    Rater.Rating = Rating;
+                    DB.RatingDatabases.Update(Rater);
+                }
+                //Else, add a new entry to the database.
+                else
+                {
+                    DB.RatingDatabases.Add(new RatingDatabase(UserRating, Rating, UserToBeRated));
+                }
+                //When in doubt, Try / Catch! This is here for when an incorrect UserID is input either as the Rater or the Ratee. 
+                //Of course, it catches other errors too (That aren't already specified above! Those *SHOULD* already be handled)
+                try
+                {
+                    DB.SaveChanges();
+                }
+                catch 
+                {
+                    return RedirectToPage("../Index");
+                }
             }
-            //Else, add a new entry to the database.
-            else 
-            {
-                DB.RatingDatabases.Add(new RatingDatabase(UserRating, Rating, UserToBeRated));
-            }
-            DB.SaveChanges();
-
             return Page();
         }
     }
--- OurCarZ/Startup.cs
+++ OurCarZ/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OurCarZ.Model;
 using OurCarZ.Services;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,7 @@ namespace OurCarZ
         {
             services.AddRazorPages();
             services.AddSingleton<IUserPersistence, UserPersistence>();
+            services.AddSingleton<EmilDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
