35a5c7086b2c6320fbd74896fe5cf2edf85fd8fd
Emil <emil377g@edu.easj.dk>
2021-05-08 22:47:06 +0200

    Jeg fuckede noget op før vi gik i fredags, men jeg har fikset det! Sorri thomas

--- OurCarZ/Pages/Routes.cshtml.cs
+++ OurCarZ/Pages/Routes.cshtml.cs
@@ -17,13 +17,6 @@ namespace OurCarZ.Pages
 
         public EmilDbContext DB = new EmilDbContext();
 
-        public List<Route> routes = new List<Route>()
-        {
-            //new Route(1, "Ahornlunden 5, 4000 Roskilde, Dk","humle 2, 4000 Roskilde, Dk"),
-            //new Route(2, "Århus 5000","Maglegårdsvej 2, 5000 Roskilde, Dk"),
-            //new Route(3, "Roskilde 4000, Dk","Maglegårdsvej 2, 4000 Roskilde, Dk"),
-            //new Route(4, "København 6000","sngel, 4000 Roskilde, Dk")
-        };
         public List<Route> UsedRoutes = new List<Route>();
 
         public List<Address> addresses;
@@ -61,17 +54,19 @@ namespace OurCarZ.Pages
 
             if (!string.IsNullOrEmpty(ZipCode))
             {
-                              
+
                 UsedRoutes = new List<Route>();
 
                 foreach (var route in allRoutes)
                 {
-                    if(addresses.Where(a => (a.AddressId == route.StartPoint || a.AddressId == route.FinishPoint) && a.ZipCode.ToString().Contains(ZipCode)).FirstOrDefault() != null) {
+                    if (addresses.Where(a => (a.AddressId == route.StartPoint || a.AddressId == route.FinishPoint) && a.ZipCode.ToString().Contains(ZipCode)).FirstOrDefault() != null)
+                    {
                         UsedRoutes.Add(route);
                     }
                 }
 
-            } else { UsedRoutes = allRoutes; }
+            }
+            else { UsedRoutes = allRoutes; }
 
             if (date != null)
             {
@@ -83,18 +78,8 @@ namespace OurCarZ.Pages
                     }
                 }
             }
-                //var sortedRoutes = routes.Where(r => r.StartPoint.Contains(ZipCode) || r.FinishPoint.Contains(ZipCode));
-                UsedRoutes = new List<Route>();
-                //foreach (var item in sortedRoutes)
-                //{
-                //    UsedRoutes.Add(item);
-                //}
-            } else { UsedRoutes = routes; }
- 
 
-            
+
         }
-
-
     }
 }
