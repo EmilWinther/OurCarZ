13b88c5bed56167df3991bcc4a88e81aefaa2a87
MadsBruhn <mads80i4@edu.easj.dk>
2021-05-10 22:15:14 +0200

    hurtigt fix af message  system

--- OurCarZ/Pages/MyMessages.cshtml.cs
+++ OurCarZ/Pages/MyMessages.cshtml.cs
@@ -4,35 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using OurCarZ.Model;
 
 namespace OurCarZ.Pages
 {
-    public class MessageModel : PageModel
+    public class MyMessagesModel : PageModel
     {
-        private readonly ILogger<ProfileModel> _logger;
+        private readonly EmilDbContext DB = new EmilDbContext();
+        private readonly IConfiguration Configuration;
 
-        public EmilDbContext DB = new EmilDbContext();
-        [BindProperty]
-        public User currentUser { get; set; }
-
-        public User FoundUser { get; set; }
-
-        public List<User> UserList { get; set; }
-        public Message msg { get; set; }
-
-        public MessageModel(ILogger<ProfileModel> logger, EmilDbContext db)
+        public MyMessagesModel(EmilDbContext db, IConfiguration configuration)
         {
-            _logger = logger;
             DB = db;
+            Configuration = configuration;
         }
 
+        [BindProperty]
+        public User currentUser { get; set; }
+        public User FoundUser { get; set; }
+        public Message msg { get; set; }
+        public List<User> UserList { get; set; }
+        public string CurrentSort { get; set; }
 
-        public void OnGet(int id)
+        public PaginatedList<Message> Messages { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, int? pageIndex, int id)
         {
             currentUser = DB.Users.Find(id);
             UserList = DB.Users.ToList();
+
+            CurrentSort = sortOrder;
+
+            IQueryable<Message> messageList = from s in DB.Messages
+                                             select s;
+
+            IQueryable<User> userList = from u in DB.Users
+                select u;
+
+            switch (sortOrder)
+            {
+                default:
+                    messageList = messageList.Where(s => s.MessageFrom.Equals(currentUser.UserId)
+                                                           || s.MessageTo.Equals(currentUser.UserId));
+                    messageList = messageList.OrderByDescending(s => s.MessageId);
+                    break;
+            }
+
+            var pageSize = Configuration.GetValue("PageSize", 10);
+            Messages = await PaginatedList<Message>.CreateAsync(
+                messageList.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
 
         public IActionResult OnPost(int id, int id2, int DeleteID)
@@ -48,6 +70,5 @@ namespace OurCarZ.Pages
 
             return RedirectToPage("/MyMessages", new { id = currentUser.UserId });
         }
-
     }
 }
