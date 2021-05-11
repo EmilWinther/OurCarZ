88d801a3a70c48da28d53aad5af06f2073bc2f6e
MadsBruhn <mads80i4@edu.easj.dk>
2021-05-11 12:32:12 +0200

    Added Delete Profile

--- OurCarZ/Pages/Profile.cshtml.cs
+++ OurCarZ/Pages/Profile.cshtml.cs
@@ -14,6 +14,7 @@ namespace OurCarZ.Pages
         private readonly ILogger<ProfileModel> _logger;
         public EmilDbContext DB = new EmilDbContext();
         public User currentUser { get; set; }
+        public RatingDatabase RatingUser { get; set; }
         public double? avg { get; set; }
 
         public ProfileModel(ILogger<ProfileModel> logger, EmilDbContext db)
@@ -27,6 +28,37 @@ namespace OurCarZ.Pages
             currentUser = DB.Users.Find(id);
             var reviews = (from x in DB.RatingDatabases where x.UserRatedId.Equals(id) select x).ToList();
             avg = (from x in reviews select x.Rating).Average();
+
+        }
+
+        public IActionResult OnPost(int DeleteID)
+        {
+            currentUser = DB.Users.Find(DeleteID);
+
+            if (avg > 0)
+            {
+                var RatingUser = DB.RatingDatabases.Where(x => x.UserRatedId == DeleteID || x.UserRatingId == DeleteID).First();
+                DB.RatingDatabases.Remove(RatingUser);
+            }
+            
+
+            var MessageUser = DB.Messages.Where(x => x.MessageFrom == DeleteID || x.MessageTo == DeleteID).ToList();
+            foreach (var message in MessageUser)
+            {
+                DB.Messages.Remove(message);
+            }
+
+            if (currentUser.LicensePlate != null)
+            {
+                var UserLicensePlate = DB.Cars.Where(x => x.LicensePlate == currentUser.LicensePlate).First();
+                DB.Cars.Remove(UserLicensePlate);
+            }
+
+
+            DB.Users.Remove(currentUser);
+            DB.SaveChanges();
+
+            return RedirectToPage("Index");
         }
     }
 }
