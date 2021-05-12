5199f5b941942f8b27d228f1b195af834ef17513
Emil <emil377g@edu.easj.dk>
2021-05-12 09:27:15 +0200

    Doesn't work properly yet.

--- OurCarZ/Pages/MyRoutes.cshtml.cs
+++ OurCarZ/Pages/MyRoutes.cshtml.cs
@@ -11,8 +11,10 @@ namespace OurCarZ.Pages
     public class MyRoutesModel : PageModel
     {
         public List<Route> myRoutes { get; set; }
+        public Route CurrentRoute { get; set; }
         public EmilDbContext DB = new EmilDbContext();
         public List<Address> AllAddresses { get; set; }
+        public Address CurrentStartAddress { get; set; }
         [BindProperty]
         public User CurrentUser { get; set; }
 
@@ -22,6 +24,23 @@ namespace OurCarZ.Pages
             CurrentUser = DB.Users.Find(userId);
             myRoutes = DB.Routes.ToList();
             AllAddresses = DB.Addresses.ToList();
+        }
+
+        public IActionResult OnPost(int DeleteRid, int DeleteAid, int userId)
+        {
+            CurrentUser = DB.Users.Find(userId);
+            var DeleteRoute2 = DB.UserRoutes.Where(x => x.RouteId == DeleteRid).First();
+            DB.UserRoutes.Remove(DeleteRoute2);
+
+            var DeleteAddress = DB.Addresses.Where(x => x.AddressId == DeleteAid).ToList();
+            foreach (var addresses in DeleteAddress)
+            {
+                DB.Addresses.Remove(addresses);
+            }
+
+            DB.UserRoutes.Remove(DeleteRoute2);
+            DB.SaveChanges();
+            return RedirectToPage("/MyRoutes", new { id = CurrentUser.UserId });
         }
     }
 }
