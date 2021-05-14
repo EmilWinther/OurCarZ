7508ca9dff0a9385b515aa38977f15765813f765
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-05-14 10:34:46 +0200

    Selecting a (model) Route in (page) Routes will now show you the (model) Route on the (page) Route. If no (model) Route was found based on the ID provided, (page) Route will now show you the default "Roskilde, DK to København, DK" (actual) Route.

--- OurCarZ/Pages/Route.cshtml.cs
+++ OurCarZ/Pages/Route.cshtml.cs
@@ -5,25 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Routing;
+using OurCarZ.Model;
 
 namespace OurCarZ.Pages
 {
     public class RouteModel : PageModel
     {
+        private EmilDbContext DB;
         [BindProperty]
-        public Route route { get; set; }
+        public Model.Route route { get; set; }
         [BindProperty]
         public string StartPoint { get; set; }
         [BindProperty]
         public string FinishPoint { get; set; }
-
+        public RouteModel(EmilDbContext edb) 
+        {
+            DB = edb;
+        }
         public void OnGet(int id)
         {
-            //Ret nedenstående til den rigtige kode
-            //route = RouteService.GetOneRoute(id);
-            //route.StartPoint = StartPoint;
-            //route.FinishPoint = FinishPoint;
-
+            route = DB.Routes.Find(id);
+            if (route != null) 
+            {
+                StartPoint = DB.Addresses.Find(route.StartPoint).RoadName;
+                FinishPoint = DB.Addresses.Find(route.FinishPoint).RoadName;
+            }
+            
         }
 
     }
