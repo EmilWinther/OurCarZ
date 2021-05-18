139dd0e266cd1b8dc5cd7168a86613190bc6e365
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-05-18 14:09:16 +0200

    PickAPath *SHOULD* now require a logged in user, and also *SHOULD* save stuff to database, unless you didn't type anything. UserRoute tables UserRouteId doesn't have identity though, so either it needs to be removed OR needs to identity, or this page is just gonna keep dumping Id = 0.

--- OurCarZ/Pages/PickAPath.cshtml.cs
+++ OurCarZ/Pages/PickAPath.cshtml.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OurCarZ.Model;
+using OurCarZ.Pages.UserPages;
 
 namespace OurCarZ.Pages
 {
     public class PickAPathModel : PageModel
     {
         private EmilDbContext _edb;
+        public User currentUser { get; set; }
         public PickAPathModel(EmilDbContext edb)
         {
             _edb = edb;
@@ -23,6 +26,9 @@ namespace OurCarZ.Pages
         public Address EndAddress { get; set; }
         [BindProperty]
         public Address PhotopathAdr { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Zipcode is required."), RegularExpression(@"^(\d{4})$", ErrorMessage = "Enter a valid 4 digit ZipCode")]
+        public int ZipCode { get; set; }
         public List<string> ExtraStops { get; set; }
         public List<int?> XtraStopsDupe { get; set; }
         public string CombineZipAndRoad(int? id)
@@ -58,8 +64,12 @@ namespace OurCarZ.Pages
             }
         }
 
-        public void OnGet(int id)
+        public IActionResult OnGet(int id)
         {
+            if (LogInPageModel.LoggedInUser != null)
+            {
+                currentUser = _edb.Users.Find(UserPages.LogInPageModel.LoggedInUser.UserId);
+            
             Route = _edb.Routes.Find(id);
             if (Route != null)
             {
@@ -67,16 +77,42 @@ namespace OurCarZ.Pages
                 EndAddress = _edb.Addresses.Find(Route.FinishPoint);
                 ExtraWaypoints(Route.RouteId);
             }
+            }
+            return Page();
         }
         public IActionResult OnPost(int id) 
         {
+            PhotopathAdr.ZipCode = ZipCode;
             OnGet(id);
             //Do you like it?
             return Page();
         }
         public IActionResult OnPostRequest()
         {
-            return RedirectToPage("Index");
+            if (PhotopathAdr.RoadName != null || ZipCode != 0)
+            {
+                PhotopathAdr.ZipCode = ZipCode;
+                if (!_edb.Addresses.Any(x => x.RoadName == PhotopathAdr.RoadName))
+                {
+                    _edb.Addresses.Add(PhotopathAdr);
+                    _edb.SaveChanges();
+                }
+                UserRoute ur = new UserRoute();
+                ur.RouteId = Route.RouteId;
+                ur.UserId = _edb.Users.Find(UserPages.LogInPageModel.LoggedInUser.UserId).UserId;
+                ur.Via = _edb.Addresses.FirstOrDefault(x => x.RoadName == PhotopathAdr.RoadName).AddressId;
+
+                _edb.UserRoutes.Add(ur);
+                _edb.SaveChanges();
+
+                return RedirectToPage("Index");
+            }
+            else 
+            {
+                OnGet(Route.RouteId);
+                return Page();
+            }
+
         }
     }
 }
