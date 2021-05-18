f9c1de4cd72a760222bc2d654b9c447b6bc4f403
MadsBruhn <mads80i4@edu.easj.dk>
2021-05-18 12:48:55 +0200

    Past Routes Done

--- OurCarZ/Pages/Past Routes.cshtml.cs
+++ OurCarZ/Pages/Past Routes.cshtml.cs
@@ -9,7 +9,6 @@ namespace OurCarZ.Pages
 {
     public class Past_RoutesModel : PageModel
     {
-        [BindProperty] public string ZipCode { get; set; }
         [BindProperty] public DateTime date { get; set; }
         [BindProperty]
         public User CurrentUser { get; set; }
@@ -51,37 +50,15 @@ namespace OurCarZ.Pages
             {
                 RouteList.Add(route.RouteId);
             }
-
         }
 
 
         public void OnPost()
         {
-            ZipCode = Request.Form["ZipCodeSearch"];
-            if (Request.Form["dateOfRoute"].Count > 0) date = Convert.ToDateTime(Request.Form["dateOfRoute"]);
-
             var allRoutes = DB.Routes.ToList();
             addresses = DB.Addresses.ToList();
             users = DB.Users.ToList();
             cars = DB.Cars.ToList();
-
-            if (!string.IsNullOrEmpty(ZipCode))
-            {
-                UsedRoutes = new List<Route>();
-
-                foreach (var route in allRoutes.Where(route => addresses.FirstOrDefault(a => (a.AddressId == route.StartPoint || a.AddressId == route.FinishPoint) &&
-                    a.ZipCode.ToString().Contains(ZipCode)) != null))
-                    UsedRoutes.Add(route);
-            }
-            else
-            {
-                UsedRoutes = allRoutes;
-            }
-
-            if (date != null)
-                foreach (var route in UsedRoutes.ToList())
-                    if (DateTime.Compare(route.StartTime, date) != 0)
-                        UsedRoutes.Remove(route);
         }
     }
 }
