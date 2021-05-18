493ff02c297e301c95ea98d8a6ab9bbbd2e1e68c
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-05-18 10:34:33 +0200

    PickAPath can now display a route, along with all the extra stopping points along that route, and you can add an extra stopping point for that route, but it won't get saved and you get booted to Index instead. 'sfine.

--- OurCarZ/Pages/PickAPath.cshtml.cs
+++ OurCarZ/Pages/PickAPath.cshtml.cs
@@ -11,8 +11,6 @@ namespace OurCarZ.Pages
     public class PickAPathModel : PageModel
     {
         private EmilDbContext _edb;
-        
-
         public PickAPathModel(EmilDbContext edb)
         {
             _edb = edb;
@@ -24,7 +22,41 @@ namespace OurCarZ.Pages
         [BindProperty]
         public Address EndAddress { get; set; }
         [BindProperty]
-        public string Lookatthisphotopath { get; set; }
+        public Address PhotopathAdr { get; set; }
+        public List<string> ExtraStops { get; set; }
+        public List<int?> XtraStopsDupe { get; set; }
+        public string CombineZipAndRoad(int? id)
+        {
+            string returner;
+            returner = _edb.Addresses.Find(id).RoadName + " " + _edb.Addresses.Find(id).ZipCode;
+            return returner;
+        }
+        public void ExtraWaypoints(int router)
+        {
+            List<int?> XtraStops;
+            XtraStops = _edb.UserRoutes.Where(x => x.RouteId == router)
+                .Select(x => x.Via).ToList();
+            XtraStopsDupe = XtraStops.Distinct().ToList();
+            if (XtraStopsDupe.Contains(Route.StartPoint))
+            {
+                XtraStopsDupe.Remove(Route.StartPoint);
+            }
+            if (XtraStopsDupe.Contains(Route.FinishPoint))
+            {
+                XtraStopsDupe.Remove(Route.FinishPoint);
+            }
+            ExtraStops = new List<string>();
+
+            List<string> ExtraStopsDupe = new List<string>();
+            foreach (int? y in XtraStopsDupe)
+            {
+                ExtraStopsDupe.Add(_edb.Addresses.Find(y).RoadName);
+            }
+            foreach (int? i in XtraStops)
+            {
+                ExtraStops.Add(_edb.Addresses.Find(i).RoadName);
+            }
+        }
 
         public void OnGet(int id)
         {
@@ -33,7 +65,18 @@ namespace OurCarZ.Pages
             {
                 StartAddress = _edb.Addresses.Find(Route.StartPoint);
                 EndAddress = _edb.Addresses.Find(Route.FinishPoint);
+                ExtraWaypoints(Route.RouteId);
             }
+        }
+        public IActionResult OnPost(int id) 
+        {
+            OnGet(id);
+            //Do you like it?
+            return Page();
+        }
+        public IActionResult OnPostRequest()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
