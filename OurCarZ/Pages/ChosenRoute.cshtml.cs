270f770493d2c84e3f61a2df1a0738dd3504e0f3
Emil <emil377g@edu.easj.dk>
2021-05-18 13:02:25 +0200

    Fully working except for seeing passengers on the route.

--- OurCarZ/Pages/ChosenRoute.cshtml.cs
+++ OurCarZ/Pages/ChosenRoute.cshtml.cs
@@ -8,14 +8,19 @@ namespace OurCarZ.Pages
 {
     public class ChosenRouteModel : PageModel
     {
+        [BindProperty]
+        public List<UserRoute> PassengerList { get; set; }
         public Route myRoute { get; set; }
-        public List<UserRoute> userRoute { get; set; }
         public EmilDbContext DB = new EmilDbContext();
         public List<Address> currentAddress { get; set; }
         public List<Address> EndAddress { get; set; }
         [BindProperty]
         public User CurrentUser { get; set; }
         public User Driver { get; set; }
+        [BindProperty]
+        public string StartPoint { get; set; }
+        [BindProperty]
+        public string FinishPoint { get; set; }
         public void OnGet(int userId, int routeId)
         {
             CurrentUser = DB.Users.Find(userId);
@@ -23,6 +28,9 @@ namespace OurCarZ.Pages
             myRoute = DB.Routes.Find(routeId);
             EndAddress = DB.Addresses.ToList();
 
+
+            StartPoint = DB.Addresses.Find(myRoute.StartPoint).RoadName;
+            FinishPoint = DB.Addresses.Find(myRoute.FinishPoint).RoadName;
             if (myRoute.UserId == myRoute.UserId)
             {
                 Driver = DB.Users.Find(myRoute.UserId);
