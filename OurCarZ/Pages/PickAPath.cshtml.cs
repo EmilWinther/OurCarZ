cfee605b99e6c8fe67f8dfa98c7c64e32b681f37
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-05-19 10:29:39 +0200

    Checks if there are more seats than passengers in a route before saving a User Route. Also comments?

--- OurCarZ/Pages/PickAPath.cshtml.cs
+++ OurCarZ/Pages/PickAPath.cshtml.cs
@@ -29,89 +29,91 @@ namespace OurCarZ.Pages
         [BindProperty]
         [Required(ErrorMessage = "Zipcode is required."), RegularExpression(@"^(\d{4})$", ErrorMessage = "Enter a valid 4 digit ZipCode")]
         public int ZipCode { get; set; }
-        public List<string> ExtraStops { get; set; }
-        public List<int?> XtraStopsDupe { get; set; }
+        public List<int?> XtraStops { get; set; }
         public string CombineZipAndRoad(int? id)
         {
+            //Get an Addresses name and zipcode based on AddressId
             string returner;
             returner = _edb.Addresses.Find(id).RoadName + " " + _edb.Addresses.Find(id).ZipCode;
             return returner;
         }
         public void ExtraWaypoints(int router)
         {
-            List<int?> XtraStops;
             XtraStops = _edb.UserRoutes.Where(x => x.RouteId == router)
                 .Select(x => x.Via).ToList();
-            XtraStopsDupe = XtraStops.Distinct().ToList();
-            if (XtraStopsDupe.Contains(Route.StartPoint))
+            //Distinct, to ensure no dupes.
+            XtraStops = XtraStops.Distinct().ToList();
+            //remove start and end point for XtraStops, the maps library we used doesn't like dupe addresses :c
+            if (XtraStops.Contains(Route.StartPoint))
             {
-                XtraStopsDupe.Remove(Route.StartPoint);
+                XtraStops.Remove(Route.StartPoint);
             }
-            if (XtraStopsDupe.Contains(Route.FinishPoint))
+            if (XtraStops.Contains(Route.FinishPoint))
             {
-                XtraStopsDupe.Remove(Route.FinishPoint);
-            }
-            ExtraStops = new List<string>();
-
-            List<string> ExtraStopsDupe = new List<string>();
-            foreach (int? y in XtraStopsDupe)
-            {
-                ExtraStopsDupe.Add(_edb.Addresses.Find(y).RoadName);
-            }
-            foreach (int? i in XtraStops)
-            {
-                ExtraStops.Add(_edb.Addresses.Find(i).RoadName);
+                XtraStops.Remove(Route.FinishPoint);
             }
         }
 
         public IActionResult OnGet(int id)
         {
+            //Check, if the user is logged.
             if (LogInPageModel.LoggedInUser != null)
             {
+                //Sets the currentUser as the loggedin user, used later for adding UserId unto the chosen Route, along with the extra pick-up point.
                 currentUser = _edb.Users.Find(UserPages.LogInPageModel.LoggedInUser.UserId);
             
-            Route = _edb.Routes.Find(id);
-            if (Route != null)
-            {
-                StartAddress = _edb.Addresses.Find(Route.StartPoint);
-                EndAddress = _edb.Addresses.Find(Route.FinishPoint);
-                ExtraWaypoints(Route.RouteId);
-            }
+                Route = _edb.Routes.Find(id);
+                //check if the route isn't null (just in case)
+                if (Route != null)
+                {
+                    StartAddress = _edb.Addresses.Find(Route.StartPoint);
+                    EndAddress = _edb.Addresses.Find(Route.FinishPoint);
+                    ExtraWaypoints(Route.RouteId);
+                }
             }
             return Page();
         }
         public IActionResult OnPost(int id) 
         {
             PhotopathAdr.ZipCode = ZipCode;
+            //OnGet is called to ensure OnGet is run, when Page is reloaded. Otherwise, it doesn't!
+            //(Last this was checked, without this line additional stops for a Route doesn't get loaded, only the base Route.)
             OnGet(id);
-            //Do you like it?
             return Page();
         }
         public IActionResult OnPostRequest()
         {
+            Route = _edb.Routes.Find(Route.RouteId);
+            //check to see if values are typed in the input fields
             if (PhotopathAdr.RoadName != null || ZipCode != 0)
             {
-                PhotopathAdr.ZipCode = ZipCode;
-                if (!_edb.Addresses.Any(x => x.RoadName == PhotopathAdr.RoadName))
+                //check to see if there are less participants in a route, than there are seats in the car driving them.
+                if (Convert.ToInt32(_edb.Cars.Find(_edb.Users.Find(Route.UserId).LicensePlate).Seats) > _edb.UserRoutes.Where(x => x.RouteId == Route.RouteId).Select(x => x.Via).ToList().Count) 
                 {
-                    _edb.Addresses.Add(PhotopathAdr);
+                    PhotopathAdr.ZipCode = ZipCode;
+                    //add an address, if it doesn't exist in the address table
+                    if (!_edb.Addresses.Any(x => x.RoadName == PhotopathAdr.RoadName))
+                    {
+                        _edb.Addresses.Add(PhotopathAdr);
+                        _edb.SaveChanges();
+                    }
+                    //Adds the users pickup point to the route.
+                    UserRoute ur = new UserRoute();
+                    ur.RouteId = Route.RouteId;
+                    ur.UserId = _edb.Users.Find(UserPages.LogInPageModel.LoggedInUser.UserId).UserId;
+                    ur.Via = _edb.Addresses.FirstOrDefault(x => x.RoadName == PhotopathAdr.RoadName).AddressId;
+
+                    _edb.UserRoutes.Add(ur);
                     _edb.SaveChanges();
+
+                    //Boots to index. Could be a little nicer...
+                    return RedirectToPage("Index");
                 }
-                UserRoute ur = new UserRoute();
-                ur.RouteId = Route.RouteId;
-                ur.UserId = _edb.Users.Find(UserPages.LogInPageModel.LoggedInUser.UserId).UserId;
-                ur.Via = _edb.Addresses.FirstOrDefault(x => x.RoadName == PhotopathAdr.RoadName).AddressId;
-
-                _edb.UserRoutes.Add(ur);
-                _edb.SaveChanges();
-
-                return RedirectToPage("Index");
             }
-            else 
-            {
-                OnGet(Route.RouteId);
-                return Page();
-            }
+            //OnGet is called to ensure OnGet is run, when Page is reloaded. Otherwise, it doesn't!
+            //(Last this was checked, without this line additional stops for a Route doesn't get loaded, only the base Route.)
+            OnGet(Route.RouteId);
+            return Page();
 
         }
     }
