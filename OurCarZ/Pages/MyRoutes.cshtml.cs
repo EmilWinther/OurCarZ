91bbc26ada0cf29ab7efdd7257da7cd56b1012bd
MadsBruhn <mads80i4@edu.easj.dk>
2021-05-20 10:46:06 +0200

    my routes passenger quit done og seawts i routes

--- OurCarZ/Pages/MyRoutes.cshtml.cs
+++ OurCarZ/Pages/MyRoutes.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using OurCarZ.Model;
 using System.Collections.Generic;
 using System.Linq;
+using OurCarZ.Pages.UserPages;
 
 namespace OurCarZ.Pages
 {
@@ -14,12 +15,13 @@ namespace OurCarZ.Pages
         public EmilDbContext DB = new EmilDbContext();
         public List<Address> AllAddresses { get; set; }
         public Address CurrentStartAddress { get; set; }
-        [BindProperty]
-        public User CurrentUser { get; set; }
+        [BindProperty] public User CurrentUser { get; set; }
+        public List<UserRoute> PassengerRoutes { get; set; }
 
 
         public void OnGet(int userId)
         {
+            PassengerRoutes = DB.UserRoutes.ToList();
             CurrentUser = DB.Users.Find(userId);
             myRoutes = DB.Routes.ToList();
             AllAddresses = DB.Addresses.ToList();
@@ -37,9 +39,24 @@ namespace OurCarZ.Pages
             {
                 DB.Routes.Remove(route);
             }
+            
+            DB.SaveChanges();
+            return RedirectToPage("/MyRoutes", new {userId = LogInPageModel.LoggedInUser.UserId});
+        }
+
+        public IActionResult OnPostRemove(int RemoveRid, int removeId)
+        {
+            var RoutePassenger = DB.UserRoutes.Where(p => p.RouteId == RemoveRid).ToList();
+            foreach (var userRoute in RoutePassenger)
+            {
+                if (userRoute.UserId == LogInPageModel.LoggedInUser.UserId)
+                {
+                    DB.UserRoutes.Remove(userRoute);
+                }
+            }
 
             DB.SaveChanges();
-            return RedirectToPage("/MyRoutes", new { userId = CurrentUser.UserId });
+            return RedirectToPage("/MyRoutes", new {userId = LogInPageModel.LoggedInUser.UserId});
         }
     }
 }
