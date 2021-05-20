18a4dba4b601f9ebf5889cf145d9c67fa586fea3
Nicolas Lauridsen <nicolas.lauridsen@gmail.com>
2021-05-20 12:41:05 +0200

    Rating available on YourProfile, also the css shouldn't mess the header up anymore (?)

--- OurCarZ/Pages/Your Profile.cshtml.cs
+++ OurCarZ/Pages/Your Profile.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using OurCarZ.Model;
@@ -10,11 +11,15 @@ namespace OurCarZ.Pages
     {
         private readonly ILogger<YourProfileModel> _logger;
         public EmilDbContext DB = new EmilDbContext();
-        public User FoundUser { get; set; }
-        public User CurrentUser { get; set; }
         public RatingDatabase Rating { get; set; }
         public RatingDatabase UserRated { get; set; }
         public double? avg { get; set; }
+        [BindProperty]
+        public int UsersRating { get; set; }
+        [BindProperty]
+        public User FoundUser { get; set; }
+        [BindProperty]
+        public User CurrentUser { get; set; }
 
         public YourProfileModel(ILogger<YourProfileModel> logger, EmilDbContext db)
         {
@@ -33,6 +38,33 @@ namespace OurCarZ.Pages
                 avg = (from x in reviews select x.Rating).Average();
             }
 
+        }
+        public IActionResult OnPostRate() 
+        {
+            //Making sure nothing is zero, because it will break the program if so. Luckily, the integers will also return 0
+            //if an illegal character is input on the page (such as letters)
+            if (FoundUser.UserId != 0 && CurrentUser.UserId != 0 && UsersRating != 0)
+            {
+                //Check if the specified Composite Key already exists in the Database and update it
+                if (DB.RatingDatabases.Find(FoundUser.UserId, CurrentUser.UserId) != null)
+                {
+                    var Rater = DB.RatingDatabases.FirstOrDefault(b => b.UserRatedId == FoundUser.UserId && b.UserRatingId == CurrentUser.UserId);
+                    Rater.Rating = UsersRating;
+                    DB.RatingDatabases.Update(Rater);
+                }
+                //Else, add a new entry to the database.
+                else
+                {
+                    RatingDatabase newRating = new RatingDatabase();
+                    newRating.UserRatedId = FoundUser.UserId;
+                    newRating.UserRatingId = CurrentUser.UserId;
+                    newRating.Rating = UsersRating;
+                    DB.RatingDatabases.Add(newRating);
+                }
+                DB.SaveChanges();
+            }
+            OnGet(FoundUser.UserId);
+            return Page();
         }
     }
 }
